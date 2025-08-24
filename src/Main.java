44672569916a53919bd26e8a5c82a9d230215d01
Olesia_Dunaevskaia <o.v.lavrova@yandex.ru>
2025-08-24 18:05:50 +0400

    Now it can count sum, difference, multiplication and quotient of two numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: " );
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = new Scanner(System.in).nextInt();
+
+        int sum = number1 + number2;
+        System.out.println("Сумма: " + sum);
+
+        int difference = number1 - number2;
+        System.out.println("Разность: " + difference);
+
+        int multiplication = number1 * number2;
+        System.out.println("Произведение: " + multiplication);
+
+        double quotient = (double) number1 / number2;
+        System.out.println("Частное: " + quotient);
+
     }
 }
